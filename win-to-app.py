15894493aa8df24b202ad3adfa016c4702eb6b64
Sam Adam-Day <me@samadamday.com>
2014-09-20 13:50:35 +0100

    Switched to OOP

--- win-to-app.py
+++ win-to-app.py
@@ -20,85 +20,107 @@ class GenericError(Exception):
 	"""A generic exception"""
 	pass
 
+class XServerError(Exception):
+	"""An exception raised when there's a problem with the X Server"""
+	def __init__(self,error):
+		self.error = error
+
 class ApplicationNotFoundError(Exception):
 	"""The exception raised when a the .desktop file for a given window ID could not be found."""
 	pass
 
 
-def get_xdg_application_files():
-	"""Provide a list of the application files, with full paths.
-	Specification: http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html"""
-	files = []
-	# Loop over the directories in $XDG_DATA_DIRS (essentially; see xdg doc for info)
-	for directory in BaseDirectory.load_data_paths("applications"):
-		files.extend([os.path.join(directory,filename) for filename in os.listdir(directory)])
-	return files
+class WinToApp:
+	"""The main class for handling window to application requests.
+
+	Usage:
+
+	    wta = WinToApp()
+	    applicationPath = wta.from_id(windowId)
+	"""
 
 
-def win_to_app(id):
-	"""Try to determine the .desktop file the X window ID given by `id`. 
+	def __init__(self):
+		"""Load up the applications and connect to the X Server"""
 
-	Returns the full path on success; raises ApplicationNotFound on failure. May also raise Xerror.DisplayError."""
+		# The list of .desktop files' relevant details. A list of dicts with keys "FullPath", "StartupWMClass", "Exec"
+		self.applications = []
 
-	# The list of .desktop files' relevant details. A list of dicts with keys "FullPath", "StartupWMClass", "Exec"
-	applications = []
+		## Load up all the .desktop files
+		for fullPath in self._get_xdg_application_files():
 
-	## Load up all the .desktop files
-	for fullPath in get_xdg_application_files():
+			# Make sure this is a .desktop file
+			if not os.path.isfile(fullPath) or os.path.splitext(fullPath)[1].lower() != ".desktop":
+				continue
 
-		# Make sure this is a .desktop file
-		if not os.path.isfile(fullPath) or os.path.splitext(fullPath)[1].lower() != ".desktop":
-			continue
+			# Try to parse the desktop file; catching any exceptions
+			try:
+				entry = DesktopEntry.DesktopEntry(fullPath)
+			except xdg.Exceptions.Error:
+				continue
 
-		# Try to parse the desktop file; catching any exceptions
+			# Make sure this is an application and isn't hidden (which is equivalent to not existing at all), 
+			if entry.getType() != "Application" or entry.getHidden():
+				continue
+
+			# Test the `TryExec` key, if it exists
+			if entry.getTryExec() != "" and which(entry.getTryExec()) == None:
+				continue
+
+			# Add the relevant details to the list of applications
+			self.applications.append({
+				"FullPath": fullPath,
+				"StartupWMClass": entry.getStartupWMClass(),
+				"Exec": entry.getExec()
+				})
+
+		# Connect to the X server, letting any Xerror.DisplayError pass through
 		try:
-			entry = DesktopEntry.DesktopEntry(fullPath)
-		except xdg.Exceptions.Error:
-			continue
+			self.display = Xdisplay.Display()
+		except Xerror.DisplayError as exception:
+			raise Xerror.DisplayError
 
-		# Make sure this is an application and isn't hidden (which is equivalent to not existing at all), 
-		if entry.getType() != "Application" or entry.getHidden():
-			continue
 
-		# Test the `TryExec` key, if it exists
-		if entry.getTryExec() != "" and which(entry.getTryExec()) == None:
-			continue
+	def _get_xdg_application_files(self):
+		"""Provide a list of the application files, with full paths.
+		Specification: http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html"""
+		files = []
+		# Loop over the directories in $XDG_DATA_DIRS (essentially; see xdg doc for info)
+		for directory in BaseDirectory.load_data_paths("applications"):
+			files.extend([os.path.join(directory,filename) for filename in os.listdir(directory)])
+		return files
 
-		# Add the relevant details to the list of applications
-		applications.append({
-			"FullPath": fullPath,
-			"StartupWMClass": entry.getStartupWMClass(),
-			"Exec": entry.getExec()
-			})
 
-	# Connect to the X server, letting any Xerror.DisplayError pass through
-	display = Xdisplay.Display()
+	def from_id(self,id):
+		"""Try to determine the .desktop file the X window ID given by `id`. 
 
-	# Create a resource for the window given by `id`
-	display.create_resource_object(int(id,0))
+		Returns the full path on success; raises ApplicationNotFound on failure."""
 
-	# TODO: Check if that is a valid window
+		# Create a resource for the window given by `id`
+		window = self.display.create_resource_object(int(id,0))
 
-	## Get the PID associated with the window using _NET_WM_PID, if it is well behaved
-	try:
+		# TODO: Check if that is a valid window
 
-		# Intern the atom named '_NET_WM_PID'
-		atom = display.intern_atom("_NET_WM_PID",True)
+		## Get the PID associated with the window using _NET_WM_PID, if it is well behaved
+		try:
 
-		# Make sure the atom corresponding to _NET_WM_PID exists already. It really should.
-		if atom == X.NONE:
-			raise GenericError
+			# Intern the atom named '_NET_WM_PID'
+			atom = display.intern_atom("_NET_WM_PID",True)
 
-		# Get the property response, using the type Xatom.CARDINAL
-		# This is a Xlib.protocol.request.GetProperty object, derived from Xlib.protocol.rq.ReplyRequest. See http://svad.uk/e/
-		response = window.get_full_property(atom,Xatom.CARDINAL) 
+			# Make sure the atom corresponding to _NET_WM_PID exists already. It really should.
+			if atom == X.NONE:
+				raise GenericError
 
-		 # If the application isn't well behaved, then the _NET_WM_PID won't be set :(
-		if response == None:
-			raise GenericError
+			# Get the property response, using the type Xatom.CARDINAL
+			# This is a Xlib.protocol.request.GetProperty object, derived from Xlib.protocol.rq.ReplyRequest. See http://svad.uk/e/
+			response = window.get_full_property(atom,Xatom.CARDINAL) 
 
-		# Otherwise, yay!
-		pid = int(response.value[0])
+			 # If the application isn't well behaved, then the _NET_WM_PID won't be set :(
+			if response == None:
+				raise GenericError
 
-	except GenericError:
-		pid = None
+			# Otherwise, yay!
+			pid = int(response.value[0])
+
+		except GenericError:
+			pid = None
